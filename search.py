d99b269b2fae55c811c97b0ba3eabf48a74ab583
SofiaAlbuquerque <ana.sofia.albuquerque@hotmail.com>
2018-12-05 19:43:18 +0000

    Ultimas alteracoes

--- search.py
+++ search.py
@@ -1,3 +1,10 @@
+# Ah hoc search
+# Group 7
+#
+# Sofia Albuquerque 84581
+# Marco Afonso 84610
+# João Vizoso 85251
+
 from collections import defaultdict
 from whoosh.index import create_in
 from whoosh.index import open_dir
@@ -7,8 +14,6 @@ from whoosh.qparser import *
 import csv
 import sys
 import nltk
-
-import matplotlib.pyplot as plt
 
 csv.field_size_limit(sys.maxsize)
 
@@ -36,9 +41,11 @@ def search_over_index(keywords):
     keys = nltk.word_tokenize(keywords)
     manifestos_list = []
     party_mentions_keyword = defaultdict(int)
+    top_mentions = defaultdict(int)
+
 
     ix = open_dir("indexdir")
-    with ix.searcher(weighting=scoring.TF_IDF()) as searcher:
+    with ix.searcher(weighting=scoring.BM25F()) as searcher:
         for k in keys:
             qp = QueryParser('text', ix.schema, group=OrGroup).parse(k)
             results = searcher.search(qp, limit=None)
@@ -47,6 +54,7 @@ def search_over_index(keywords):
                 if r['id'] not in manifestos_list:
                     manifestos_list.append(r['id'])
                 party_mentions_keyword[(r['party'], k)] += 1
+                top_mentions[k] += 1
 
     print("\nAll the manifestos containing the keywords:")
     for manifesto in manifestos_list:
@@ -54,9 +62,10 @@ def search_over_index(keywords):
 
     print("\nHow many times each party mentions each keyword:")
     for key, value in party_mentions_keyword.items():
-        print('{0:30}{1:40}{2:30}'.format(key[1], key[0], value))
+        print('{0:30}{1:35}{2:30}'.format(key[1], key[0], value))
 
     manifestos_keywords(manifestos_list)
+    list_mentioned_keywords(top_mentions)
 
 
 def manifestos_keywords(manifestos_list):
@@ -76,19 +85,19 @@ def manifestos_keywords(manifestos_list):
 
     print("\nFor each party how many manifestos are in the results returned:")
     for key, value in final_dict.items():
-        print('{0:40}{1:40}'.format(key, value))
+        print('{0:35}{1:35}'.format(key, value))
 
 
-def main():
+def list_mentioned_keywords(top_mentions):
+    print("\nNumber of times each keyword is mentioned:")
+    for key, value in top_mentions.items():
+        print('{0:35}{1:35}'.format(key, value))
+
+
+if __name__ == "__main__":
+    print('Processamento e Recuperação de Informação - Instituto Superior Tecnico / Universidade Lisboa')
+    print('Ad hoc search on the collection of documents\n')
     var = input("Please enter something: ")
 
-    keywords = var
     index_documents()
-    search_over_index(keywords)
-
-
-#keywords = 'force injustice'
-#index_documents()
-#search_over_index(keywords)
-
-main()
+    search_over_index(var)
