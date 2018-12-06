bbf17d99eed6a8eba5992b28b8451940153ef363
joaovizoso <joao.vizoso@hotmail.com>
2018-12-06 04:29:53 +0000

    Evaluation quase completo

--- evaluation.py
+++ evaluation.py
@@ -1,107 +1,82 @@
+#Classification of documents
+# Group 7
+#
+#Sofia Albuquerque 84581
+#Marco Afonso 84610
+#João Vizoso 85251
+
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
-from sklearn.linear_model import Perceptron
-from sklearn.neighbors import KNeighborsClassifier
 
-def naiveBayes(j):
-    train_set = pd.read_csv('en_docs_clean.csv')
-    columns = ['party', 'text']
-    train_set = train_set[columns]
-    train_set.columns = ['party', 'text']
-
-    model = MultinomialNB()
-
-    vectorizer = TfidfVectorizer(max_df=0.5,stop_words='english')
-
-    X_train, X_test, y_train, y_test = train_test_split(train_set['text'], train_set['party'])
-
-    X_train_vec = vectorizer.fit_transform(X_train)
-
-    classifier = model.fit(X_train_vec, y_train)
-
-    prediction = classifier.predict(vectorizer.transform(X_test))
-    prediction_tester = classifier.predict(vectorizer.transform(['including the young and the old, men and women, those who are disabled and minority groups, including ethnic minorities and different sexual orientations']))
-    print('Predicted: ' + prediction_tester[0])
-    print('Expected: ' + train_set['party'][j])
-    print('Text being tested: ' + train_set['text'][j])
-    print(metrics.classification_report(y_test, prediction))
-
-def linearSVC(j):
-    train_set = pd.read_csv('en_docs_clean.csv')
+def linearSVC_tester(j,doc):
+    train_set = pd.read_csv(doc)
     columns = ['party', 'text']
     train_set = train_set[columns]
     train_set.columns = ['party', 'text']
 
     model = LinearSVC()
 
-    vectorizer = TfidfVectorizer(max_df=0.5, stop_words='english')
+    vectorizer = TfidfVectorizer(use_idf=False, max_df=0.90, min_df=3, norm='l2', max_features=20000)
 
-    X_train, X_test, y_train, y_test = train_test_split(train_set['text'], train_set['party'])
+    X_train, X_test, y_train, y_test = train_test_split(train_set['text'], train_set['party'], test_size = 0.2)
 
     X_train_vec = vectorizer.fit_transform(X_train)
 
     classifier = model.fit(X_train_vec, y_train)
 
-    prediction = classifier.predict(vectorizer.transform(X_test))
-    prediction_tester = classifier.predict(vectorizer.transform(['including the young and the old, men and women, those who are disabled and minority groups, including ethnic minorities and different sexual orientations']))
+    prediction_tester = classifier.predict(vectorizer.transform([str(train_set['text'][j])]))
     print('Predicted: ' + prediction_tester[0])
     print('Expected: ' + train_set['party'][j])
     print('Text being tested: ' + train_set['text'][j])
-    print(metrics.classification_report(y_test, prediction))
 
-#VER PARAMETROS
-def nearestNeighbors(j):
-    train_set = pd.read_csv('en_docs_clean.csv')
+def linearSVC(var,doc):
+    train_set = pd.read_csv(doc)
     columns = ['party', 'text']
     train_set = train_set[columns]
     train_set.columns = ['party', 'text']
 
-    model = KNeighborsClassifier()
+    model = LinearSVC()
 
-    vectorizer = TfidfVectorizer(max_df=0.5, stop_words='english')
+    X_train, X_test, y_train, y_test = train_test_split(train_set['text'], train_set['party'], test_size = 0.2)
 
-    X_train, X_test, y_train, y_test = train_test_split(train_set['text'], train_set['party'])
-
+    vectorizer = TfidfVectorizer(max_df=0.90, min_df=3, ngram_range=(1, 2))
     X_train_vec = vectorizer.fit_transform(X_train)
 
     classifier = model.fit(X_train_vec, y_train)
-
     prediction = classifier.predict(vectorizer.transform(X_test))
-    prediction_tester = classifier.predict(vectorizer.transform(['including the young and the old, men and women, those who are disabled and minority groups, including ethnic minorities and different sexual orientations']))
-    print('Predicted: ' + prediction_tester[0])
-    print('Expected: ' + train_set['party'][j])
-    print('Text being tested: ' + train_set['text'][j])
+    prediction_tester = classifier.predict(vectorizer.transform([str(var)]))
+
+    print('Party most likely to have produced such text: ' + prediction_tester[0])
     print(metrics.classification_report(y_test, prediction))
+    print("Score: " + str(metrics.accuracy_score(y_test, prediction)))
 
-def perceptron(j):
-    train_set = pd.read_csv('en_docs_clean.csv')
-    columns = ['party', 'text']
-    train_set = train_set[columns]
-    train_set.columns = ['party', 'text']
+if __name__ == "__main__":
+    print('Processamento e Recuperação de Informação - Instituto Superior Tecnico / Universidade Lisboa')
+    print('Classification of documents according to their political affiliation.\n')
+    print('\n')
+    language = input("Please choose the document language (PT/EN): ")
 
-    model = Perceptron()
+    if language == 'PT':
+        doc = 'pt_docs_clean.csv'
+        var = input("Please enter something: ")
+        if var == 'p_tester':
+            r = input("Position: ")
+            linearSVC_tester(int(r),doc)
+        else:
+            linearSVC(var,doc)
 
-    vectorizer = TfidfVectorizer(max_df=0.5, stop_words='english')
+    elif language == 'EN':
+        doc = 'en_docs_clean.csv'
+        var = input("Please enter something: ")
+        if var == 'p_tester':
+            r = input("Position: ")
+            linearSVC_tester(int(r),doc)
+        else:
+            linearSVC(var,doc)
 
-    X_train, X_test, y_train, y_test = train_test_split(train_set['text'], train_set['party'])
+    else:
+        print('Language not available')
 
-    X_train_vec = vectorizer.fit_transform(X_train)
-
-    classifier = model.fit(X_train_vec, y_train)
-
-    prediction = classifier.predict(vectorizer.transform(X_test))
-    prediction_tester = classifier.predict(vectorizer.transform(['including the young and the old, men and women, those who are disabled and minority groups, including ethnic minorities and different sexual orientations']))
-    print('Predicted: ' + prediction_tester[0])
-    print('Expected: ' + train_set['party'][j])
-    print('Text being tested: ' + train_set['text'][j])
-    print(metrics.classification_report(y_test, prediction))
-
-# VERIFICAR PARAMETROS DO TFIDF
-# VERIFICAR PARAMETROS DE CADA ALGORITMO
-# COMPARAR PERFORMANCES
-# TIRAR PRINTS PARA O RELATORIO
-# ESCOLHER ALGORITMO E FAZER MAIN/INPUT
